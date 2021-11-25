cd67d8ae5f01e8a9be0492f150b0958addf4ff49
hoeps <wolfram.hoeps@embl.de>
2021-11-25 17:00:00 +0100

    inversion simulation in place. Not well tested yet.

--- seqbuilder/R/seqbuilder_wrapper.R
+++ seqbuilder/R/seqbuilder_wrapper.R
@@ -12,13 +12,12 @@ source_here <- function(x, ...) {
 }
 
 library(optparse)
-#sourcedir = ("/Users/hoeps/PhD/projects/nahrcall/nahrchainer/seqbuilder/R/")
-#source(here::here('seqbuilder.R'))
 
 source_here('seqbuilder.R')
 source_here('seqbuilder_functions.R')
 source_here('seqplotter.R')
 source_here('pafCoordsDotPlotly.R')
+source_here('seqmodder.R')
 
 
 # sourcefiles = list.files(sourcedir)
@@ -31,20 +30,22 @@ option_list = list(
               help="Total sequence length [bp]", metavar="numeric"),
   make_option(c("-s", "--sdfile"), type="character", default=NULL,
               help="Bedfile with desired SDs", metavar="character"),
+  make_option(c("-v", "--SVfile"), type="character", default=NULL,
+              help="Textfile with SVs to model", metavar="character"),
   make_option(c("-o", "--outprefix"), type="character", default="./outputcorr/", 
               help="Output fasta", metavar="character")
   
   )
 
-
-
-
 opt <- parse_args(OptionParser(option_list=option_list))
 
 seqlen = opt$seqlen
 sdfile = opt$sdfile
+svfile = opt$SVfile
 outprefix = opt$outprefix
 
+
+# Prepare the output directories
 fadir = "../res/fa/"
 pafdir = "../res/paf/"
 plotdir = "../res/plot/"
@@ -57,22 +58,30 @@ outfasta = paste0(fadir, outprefix, '.fa')
 outpaf = paste0(pafdir, outprefix, '.paf')
 outplot1 = paste0(plotdir, outprefix, '.pdf')
 outplot2 = paste0(plotdir, outprefix, 'minimap')
+outmutfasta = paste0(fadir, outprefix, '_mut.fa')
+outmutsd = paste0(fadir, outprefix, '_mut.bed')
 
-## MAKE THE SEQUENCE
-simulate_seq(seqlen, sdfile, outfasta)
 
-# Make an exact dotplot, in case this is feasible. 
-# Otherwise (or additionally?) we are going to make a minimap2 dotplot. 
-if (seqlen <= 25000){
-  print('making exact plot.')
-  make_dotplot(outfasta, outfasta, 15, outplot1)
-} else if (seqlen > 25000){
-  print('Sequence1 is too long for exact dotplot (>25 kbp)')
-}
+# ## MAKE THE SEQUENCE
+# simulate_seq(seqlen, sdfile, outfasta)
+# 
+# # Make an exact dotplot, in case this is feasible. 
+# # Otherwise (or additionally?) we are going to make a minimap2 dotplot. 
+# if (seqlen <= 25000){
+#   print('making exact plot.')
+#   make_dotplot(outfasta, outfasta, 15, outplot1)
+# } else if (seqlen > 25000){
+#   print('Sequence1 is too long for exact dotplot (>25 kbp)')
+# }
+# 
+# ## MAKE A MINIMAP2+DOTPLOTLY dotplot
+# system(paste0("minimap2 -x asm20 -c -z400,50 -s 0 -M 0.2 -N 100 -P --hard-mask-level ", outfasta, " ", outfasta, " > ", outpaf))
+# system(paste0("./pafCoordsDotPlotly.R -i ", outpaf, 
+#                                      " -o ", outplot2,
+#                                      " -m 50 -p 10 -q 10"))
 
-## MAKE A MINIMAP2+DOTPLOTLY dotplot
-system(paste0("minimap2 -x asm20 -c -z400,50 -s 0 -M 0.2 -N 100 -P --hard-mask-level ", outfasta, " ", outfasta, " > ", outpaf))
-system(paste0("./pafCoordsDotPlotly.R -i ", outpaf, 
-                                     " -o ", outplot2,
-                                     " -m 50 -p 10 -q 10"))
+
+## MUTATE THE SEQUENCE
+mutate_seq(outfasta, sdfile, svfile, outmutfasta, outmutsd)
+
 
---
+++ seqbuilder/R/seqmodder.R
@@ -0,0 +1,122 @@
+# Whoeps, 25th Nov 2021
+library(Biostrings)
+
+debug = F
+
+mutate_seq <- function(seq_fasta, sds_bed, sv_instr_txt, outfasta, outsd){
+  
+  if (debug){
+    seq_fasta = "../res/fa/s10.fa"
+    sds_bed = "../data/sds10x.bed"
+    sv_instr_txt = "../data/svs10.txt"
+  }
+  
+  # Load both sequence
+  seqf = readDNAStringSet(seq_fasta)
+  seqname = names(seqf)
+  seq = as.character(seqf)
+  
+  # Load sd info
+  bed_colnames = c('seqname','start','end','sdname','similarity','strand')
+  sds = read.table(sds_bed); colnames(sds) = bed_colnames
+  
+  # Load Mutation instructions
+  svs = read.table(sv_instr_txt, sep='\t'); colnames(svs) = c('SD', 'SV')
+  
+  # Make every SV
+  for (n_sv in 1:dim(svs)[1]){
+    
+    seq_sds = carry_out_inv(seq, sds, svs[n_sv,])
+    seq = seq_sds[[1]]
+    sds = seq_sds[[2]]
+  }
+  
+  writeFasta(data.frame(name='sim-sequence-mutated', seq=seq),
+             filename=outfasta)
+  
+  write.table(sds, file=outsd, sep='\t', col.names=F, row.names = F, quote = F)
+  
+  print('Success! Mutated sequence and sd written.')
+}
+
+
+carry_out_inv <- function(seq, sds, sv){
+  
+  # TODO: nested repeats will be a big problem
+  
+
+  
+
+  
+  # A) Invert sequence
+  
+  # Make helpers for sequence inversion
+  sds$sd_middle = sds$start + ((sds$end - sds$start) / 2)
+  sds_specific = sds[sds$sdname == sv$SD,]
+  
+  # Check if SD is possible given SD orientation
+  stopifnot("Error: INV can not be simulated by directly oriented SDs." = 
+              all(sds_specific$strand == '-'))
+  
+  substr(seq, sds_specific[1,]$sd_middle, sds_specific[2,]$sd_middle) = 
+    as.character(reverseComplement(DNAString(substr(seq, sds_specific[1,]$sd_middle, sds_specific[2,]$sd_middle))))
+  
+  # B) Update sds table information
+  sds_revisited = sds_document_inversion(sds, sv)
+
+  
+  return(list(seq, sds_revisited))
+}
+
+sds_document_inversion <- function(sds, sv){
+  
+  # Calculating again here. 
+  sds_specific = sds[sds$sdname == sv$SD,]
+  
+  # Which SDs are buried inside the inversion of interest?
+  sds$buried_in_inv =   sds$start >= sds_specific[1,'end'] & 
+    sds$end <= sds_specific[2,'start']
+  
+  # Are there any SD positions to update?
+  if (sum(sds$buried_in_inv) > 0){
+    
+      # A) Update location of affected SDs. #
+      # Bad style ('start_new', 'end_new'), should be rewritten. 
+      sds$start_new = 0
+      sds$end_new = 0
+      
+      sds[sds$buried_in_inv==T,]$end_new = 
+        sds_specific[2,'start'] - (sds[sds$buried_in_inv==T,]$start - sds_specific[1,'end'])
+      sds[sds$buried_in_inv==T,]$start_new = 
+        sds_specific[2,'start'] - (sds[sds$buried_in_inv==T,]$end - sds_specific[1,'end'])
+      
+      sds[sds$buried_in_inv==T,]$end = sds[sds$buried_in_inv==T,]$end_new
+      sds[sds$buried_in_inv==T,]$start = sds[sds$buried_in_inv==T,]$start_new
+      
+      sds$end_new = NULL
+      sds$start_new = NULL
+      
+      
+      # B) Update orientation state #
+      flipped_pairs = 
+        unique((sds %>% group_by(sdname) %>% filter(sum(buried_in_inv)==1))$sdname)
+      
+      # Bad style, could be written better. 
+      if (dim(sds[(sds$sdname %in% flipped_pairs) & (sds$strand == '+'),])[1] > 0){
+        sds[(sds$sdname %in% flipped_pairs) & (sds$strand == '+'),]$strand = "+flip"
+      }
+      
+      if (dim(sds[(sds$sdname %in% flipped_pairs) & (sds$strand == '-'),])[1] > 0){
+        sds[(sds$sdname %in% flipped_pairs) & (sds$strand == '-'),]$strand = "+"
+      }
+      
+      if (dim(sds[(sds$sdname %in% flipped_pairs) & (sds$strand == '+flip'),])[1] > 0){
+        sds[sds$strand == '+flip',]$strand = '-'
+      }
+      
+      # Remove helpercolumns
+      sds$sd_middle = NULL
+      sds$buried_in_inv = NULL
+  }
+  return(sds)
+}
