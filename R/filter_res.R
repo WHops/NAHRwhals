ce7e6812958d28df5f0b9f7970e2092cc4926818
whops <wolfram.hoeps@yahoo.de>
2022-08-11 13:36:57 +0200

    Removing dplyr from filter_res

--- R/filter_res.R
+++ R/filter_res.R
@@ -20,7 +20,16 @@ filter_res <- function(res, threshold){
   if ((dim(res)[2]-1) == 1){
     res$n_mut = 1 - (is.na(res[,2]) + (res[,2]=='ref'))
   } else {
-    res$n_mut = (dim(res)[2]-1) - (rowSums(is.na(res[,2:dim(res)[2]]) + (res[,2:dim(res)[2]]  == 'ref') %>% replace(is.na(.), F)))
+    res$n_mut = (dim(res)[2]-1) - (
+                                  rowSums(
+                                     replace(
+                                              (res[,2:dim(res)[2]]  == 'ref'), 
+                                              is.na((res[,2:dim(res)[2]]  == 'ref')), 
+                                              T
+                                             )
+                                   )
+                                   )
+    
   }
   res$eval_accept = res$eval >= threshold
   res_sort = res[order(-res$eval_accept, res$n_mut, -res$eval ),]
@@ -29,15 +38,18 @@ filter_res <- function(res, threshold){
   return(res_sort)
 }
 
-res = data.frame(eval=c(99.688, 99.688), mut1=c('ref','ref'))
-filter_res(res, 98)
 
-res = data.frame(eval=c(99.688, 99.688, 98, 98.1), mut1=c('ref','ref', 'inv', 'del'))
-filter_res(res, 98)
-
-
-res = data.frame(eval=c(91, 95.5, 94.4, 97, 97.0), mut1=c('ref','ref', 'inv', 'del', 'dup'), mut2=c(NA,NA,'inv',NA, NA))
-filter_res(res, 98)
-
-res = data.frame(eval=c(100, 82), mut1=c('2_inv','ref'), mut2 = c('dup',NA))
-filter_res(res, 98)
+# Quick tests. 
+# res = data.frame(eval=c(99.688, 99.688), mut1=c('ref','ref'))
+# filter_res(res, 98)
+# 
+# res = data.frame(eval=c(98.688, 98.688, 99, 99.1), mut1=c('ref','ref', 'inv', 'del'))
+# filter_res(res, 98)
+# 
+# 
+# res = data.frame(eval=c(99.1, 99.5, 94.4, 97, 97.0), mut1=c('ref','ref', 'inv', 'del', 'dup'), mut2=c(NA,NA,'inv',NA, NA))
+# filter_res(res, 98)
+# 
+# res = data.frame(eval=c(100, 82), mut1=c('2_inv','ref'), mut2 = c('dup',NA))
+# filter_res(res, 98)
+  
