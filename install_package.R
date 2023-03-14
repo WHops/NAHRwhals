9aa5b9ba47d2a2702ce9f00a700b286820a034dc
whops <wolfram.hoeps@yahoo.de>
2023-03-14 21:10:22 +0100

    Adding argparse to install

--- install_package.R
+++ install_package.R
@@ -5,5 +5,11 @@ if(!require(devtools)){
     install.packages("devtools")
 }
 
+# Also this we will need for the nahrwhals.R wrapper
+if(!require(argparse)){
+    install.packages("argparse")
+}
+
+
 # Use devtools to install the package. 
 devtools::install()
