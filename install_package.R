85d4d66bf3ba4aca78f1ea2960613398ca8bfd17
whops <wolfram.hoeps@yahoo.de>
2023-03-16 14:41:45 +0100

    Cleaning up repo, removing unwanted files, small change to install_package.R

--- install_package.R
+++ install_package.R
@@ -2,12 +2,12 @@
 
 # Install devtools if not installed.
 if(!require(devtools)){
-    install.packages("devtools")
+    install.packages("devtools", repos="https://cran.us.r-project.org")
 }
 
 # Also this we will need for the nahrwhals.R wrapper
 if(!require(argparse)){
-    install.packages("argparse")
+    install.packages("argparse", repos="https://cran.us.r-project.org")
 }
 
 
