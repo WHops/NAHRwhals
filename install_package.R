ca5b8fa9af7e153cb34915d4ef7ffe4af9c83c7c
whops <wolfram.hoeps@yahoo.de>
2023-03-16 17:24:14 +0100

    Changing installation procedure...

--- install_package.R
+++ install_package.R
@@ -1,15 +1,4 @@
 #!/usr/bin/env Rscript
 
-# Install devtools if not installed.
-if(!require(devtools)){
-    install.packages("devtools", repos="https://cran.us.r-project.org")
-}
-
-# Also this we will need for the nahrwhals.R wrapper
-if(!require(argparse)){
-    install.packages("argparse", repos="https://cran.us.r-project.org")
-}
-
-
 # Use devtools to install the package. 
 devtools::install()
